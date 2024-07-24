0f0164aec3371e75989c78a4aade1c628b7c6ea1
nimrashahid20 <nimrashahid02007@gmail.com>
2024-07-24 15:18:12 +0500

    DOM(Document object Model)

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log(document)
+// console.log (document.body.childNodes)
+
+let head = document.getElementById("heading");
+head.style.color = "red";
+head.style.backgroundColor = "black";
+head.style.fontSize = "100px";
+console.log(head);
+
+let paraghraph = document.getElementsByClassName("para")
+paraghraph[0].style.color = "purple";
+paraghraph[0].style.backgroundColor = "pink";
+paraghraph[0].style.fontSize = "30px";
+paraghraph[0].style.border = "3px dotted black";
+paraghraph[1].style.color = "green";
+paraghraph[1].style.backgroundColor = "black";
+paraghraph[1].style.fontSize = "30px"
+
+paraghraph[1].innerHTML = "nimra"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
